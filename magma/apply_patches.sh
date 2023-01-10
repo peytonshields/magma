55819592958129e4d05854b9ebee78711cbbc7e9
peytonshields <peytonshields825@gmail.com>
2023-01-10 18:17:51 -0500

    updated to only apply patches in PATCHES env

--- magma/apply_patches.sh
+++ magma/apply_patches.sh
@@ -25,19 +25,15 @@ while read patch; do
 
     if [[ $PATCHES == "all patches" ]]; then
         patch -p1 -d "$TARGET/repo" <"$patch"
-    else
-        if [[ "$PATCHES" == *"$patch_name"* ]]; then
-          echo "Modifying $patch" 2>&1 | tee $OUT/patchlog.txt
-          sed -i 's/^.*+#ifdef MAGMA_ENABLE_CANARIES.*$/+#ifndef MAGMA_ENABLE_CANARIES/' "$patch"
-          sed -i 's/^.*+#ifdef MAGMA_ENABLE_FIXES.*$/+#oldifndef MAGMA_ENABLE_FIXES/' "$patch"
-          sed -i 's/^.*+#ifndef MAGMA_ENABLE_FIXES.*$/+#ifdef MAGMA_ENABLE_FIXES/' "$patch"
-          sed -i 's/^.*+#oldifndef MAGMA_ENABLE_FIXES.*$/+#ifndef MAGMA_ENABLE_FIXES/' "$patch"
-          patch --verbose -p1 -d "$TARGET/repo" <"$patch" >>  $OUT/patchlog.txt 2>&1
-
-        else
-          patch -p1 -d "$TARGET/repo" <"$patch"
-        fi
+	elif [[ "$PATCHES" == *"$patch_name"* ]]; then
+		echo "Modifying $patch" 2>&1 | tee $OUT/patchlog.txt
+		sed -i 's/^.*+#ifdef MAGMA_ENABLE_CANARIES.*$/+#ifndef MAGMA_ENABLE_CANARIES/' "$patch"
+		sed -i 's/^.*+#ifdef MAGMA_ENABLE_FIXES.*$/+#oldifndef MAGMA_ENABLE_FIXES/' "$patch"
+		sed -i 's/^.*+#ifndef MAGMA_ENABLE_FIXES.*$/+#ifdef MAGMA_ENABLE_FIXES/' "$patch"
+		sed -i 's/^.*+#oldifndef MAGMA_ENABLE_FIXES.*$/+#ifndef MAGMA_ENABLE_FIXES/' "$patch"
+		patch --verbose -p1 -d "$TARGET/repo" <"$patch" >>  $OUT/patchlog.txt 2>&1
     fi
 done
+
 cp $OUT/patchlog.txt /tmp/.
 rm $OUT/patchlog.txt
